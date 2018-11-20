f9ca4bfe25853c597b35960f67244f6491b82443
Ian Cullen <ian.james.cullen@gmail.com>
2018-11-20 08:44:55 +0000

    No need to re-refresh package content
    This creates a much faster start-up

--- init.el
+++ init.el
@@ -44,7 +44,6 @@
   (package-refresh-contents)
   (package-install 'use-package)
   )
-(package-refresh-contents)
 
 ;;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
 ( when (file-readable-p "~/.emacs.d/config.org")
