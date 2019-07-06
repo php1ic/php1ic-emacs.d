1500ebed83fc19c54cc4edbcc2cf709e6dec0353
Ian Cullen <ian.james.cullen@gmail.com>
2019-07-06 12:13:19 +0100

    Add the magit-todos package

--- init.el
+++ init.el
@@ -62,7 +62,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (async company company-jedi flycheck gitlab-ci-mode gitlab-ci-mode-flycheck irony irony-eldoc jedi-core yasnippet yaml-mode ws-butler which-key use-package switch-window swiper solarized-theme smart-mode-line rainbow-delimiters projectile org-bullets multiple-cursors modern-cpp-font-lock markdown-mode magit ido-vertical-mode ido-completing-read+ htmlize git-gutter-fringe+ flycheck-clang-analyzer flx-ido expand-region diminish dashboard company-irony company-c-headers beacon avy))))
+    (magit-todos async company company-jedi flycheck gitlab-ci-mode gitlab-ci-mode-flycheck irony irony-eldoc jedi-core yasnippet yaml-mode ws-butler which-key use-package switch-window swiper solarized-theme smart-mode-line rainbow-delimiters projectile org-bullets multiple-cursors modern-cpp-font-lock markdown-mode magit ido-vertical-mode ido-completing-read+ htmlize git-gutter-fringe+ flycheck-clang-analyzer flx-ido expand-region diminish dashboard company-irony company-c-headers beacon avy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
